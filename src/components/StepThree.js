a2d2d7b24f7b36d372f69902c266b4b9ad1d2277
unknown <aqworlds.555@gmail.com>
2023-03-07 10:27:31 +0100

    working in stepThree files

--- src/components/StepThree.js
+++ src/components/StepThree.js
@@ -1,19 +1,34 @@
+import styles from '../cssModules/stepThree.module.css'
 function StepThree(){
     return(
         <>
             <h1>Pick add-ons</h1>
             <p className="paragraph">Add-ons help enhance your gaming experience.</p>
-            Online service
-            Access to multiplayer games
-            +$1/mo
+            <button className={styles.checkLabel} >
+                <input type="checkbox" />
+                <div className={styles.addsDetails}>
+                    <h2>Online service</h2>
+                    <p>Access to multiplayer games</p>
+                </div>
+                <p className={styles.price}>+$1/mo</p>
+            </button>
 
-            Larger storage
-            Extra 1TB of cloud save
-            +$2/mo
-
-            Customizable Profile
-            Custom theme on your profile
-            +$2/mo
+            <button className={styles.checkLabel}>
+                <input type="checkbox" />
+                <div className={styles.addsDetails}>
+                    <h2>Larger storage</h2>
+                    <p>Extra 1TB of cloud save</p>
+                </div>
+                <p className={styles.price}>+$2/mo</p>
+            </button>
+            <button className={styles.checkLabel}>
+                <input type="checkbox" />
+                <div className={styles.addsDetails}>
+                    <h2>Customizable Profile</h2>
+                    <p>Custom theme on your profile</p>
+                </div>
+                <p className={styles.price}>+$2/mo</p>
+            </button>
         </>
     )
 }
